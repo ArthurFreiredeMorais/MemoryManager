4fc81e960aed5f16287fd81eed75019077a88d28
Arthur Freire de Morais <130699430+ArthurFreiredeMorais@users.noreply.github.com>
2024-03-16 20:41:40 -0300

    Update Execute.java

--- src/so/Execute.java
+++ src/so/Execute.java
@@ -2,15 +2,20 @@ package so;
 
 public class Execute {
 	public static void main(String[] args) {
-		
-		Process p1 = SystemOperation.systemCall(SystemCallType.CREAT_PROCESS, null);
+
+		Process p1 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(20, "P1"));
 		SystemOperation.systemCall(SystemCallType.WRITE_PROCESS, p1);
-		
-		Process p2 = SystemOperation.systemCall(SystemCallType.CREAT_PROCESS, null);
+		Process p2 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(38, "P2"));
 		SystemOperation.systemCall(SystemCallType.WRITE_PROCESS, p2);
-		
-		Process p3 = SystemOperation.systemCall(SystemCallType.CREAT_PROCESS, null);
+		Process p3 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(38, "P3"));
 		SystemOperation.systemCall(SystemCallType.WRITE_PROCESS, p3);
+		Process p4 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(20, "P4"));
+		SystemOperation.systemCall(SystemCallType.WRITE_PROCESS, p4);
+		Process p5 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(8, "P5"));
+		SystemOperation.systemCall(SystemCallType.WRITE_PROCESS, p5);
+		Process p6 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(40, "P5"));
+		SystemOperation.systemCall(SystemCallType.WRITE_PROCESS, p6);
+		
 	}
 
 }
