850e751a88c5badbf64ba3da27fb58556b6fbf85
Arthur Freire de Morais <130699430+ArthurFreiredeMorais@users.noreply.github.com>
2024-03-16 20:42:47 -0300

    Update SystemOperation.java

--- src/so/SystemOperation.java
+++ src/so/SystemOperation.java
@@ -6,28 +6,30 @@ import so.memory.Strategy;
 import so.scheduler.Scheduler;
 
 public class SystemOperation {
-	
 	private static MemoryManager mm;
 	private static CpuManager cm;
 	private static Scheduler scheduler;
-	
-	
+
 	public static Process systemCall(SystemCallType type, Process p) {
-		if(type.equals(SystemCallType.CREAT_PROCESS)){
-			if(mm == null) {
-				mm = new MemoryManager(Strategy.BEST_FIT);
+		if (type.equals(SystemCallType.CREATE_PROCESS)) {
+			System.out.println("Creating Process: " + p.getId());
+			if (mm == null) {
+				mm = new MemoryManager(Strategy.FIRST_FIT);
 			}
-			if(cm == null) {
+			if (cm == null) {
 				cm = new CpuManager();
+
 			}
-			return new Process();
-			
-		} else if (type.equals(SystemCallType.WRITE_PROCESS)){
+			return p;
+
+		} else if (type.equals(SystemCallType.WRITE_PROCESS)) {
 			mm.write(p);
-			
-		} else if (type.equals(SystemCallType.CLOSE_PROCESS)) {
+		}
+
+		else if (type.equals(SystemCallType.CLOSE_PROCESS)) {
 			mm.delete(p);
 		}
+
 		return null;
 	}
 
