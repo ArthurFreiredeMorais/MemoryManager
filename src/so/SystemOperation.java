643405c195fe22cc66a21e1af2100703f8f5679e
Arthur Freire de Morais <130699430+ArthurFreiredeMorais@users.noreply.github.com>
2024-03-19 13:50:11 -0300

    Update SystemOperation.java

--- src/so/SystemOperation.java
+++ src/so/SystemOperation.java
@@ -14,7 +14,7 @@ public class SystemOperation {
 		if (type.equals(SystemCallType.CREATE_PROCESS)) {
 			System.out.println("Creating Process: " + p.getId());
 			if (mm == null) {
-				mm = new MemoryManager(Strategy.FIRST_FIT);
+				mm = new MemoryManager(Strategy.WORST_FIT);
 			}
 			if (cm == null) {
 				cm = new CpuManager();
