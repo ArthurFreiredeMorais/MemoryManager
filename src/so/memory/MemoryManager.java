e97e654102001b273df67bf0db4d8b0d5c0b1c12
Arthur Freire de Morais <130699430+ArthurFreiredeMorais@users.noreply.github.com>
2024-03-19 13:49:19 -0300

    Update MemoryManager.java

--- src/so/memory/MemoryManager.java
+++ src/so/memory/MemoryManager.java
@@ -26,95 +26,78 @@ public class MemoryManager {
 
 	}
 
+	
 	private void writeWithBestFit(Process p) {
+	    int bestSize = Integer.MAX_VALUE; // MAX VALUE PARA ENCONTRAR O MELHOR AJUSTE
+	    int bestStart = -1;
 
-		int bestSize = -1;
-		int actualSize = 0;
-		int actualBestSize = 0;
-		System.out.println("Process: " + p.getSizeInMemory());
+	    int actualSize = 0;
+	    for (int i = 0; i < this.physicMemory.length; i++) {
+	        if (this.physicMemory[i] == null) {
+	            actualSize++;
+	            if (i == this.physicMemory.length - 1 || this.physicMemory[i + 1] != null) {
+	                if (actualSize >= p.getSizeInMemory() && actualSize < bestSize) {
+	                    bestSize = actualSize;
+	                    bestStart = i - actualSize + 1;
+	                }
+	                actualSize = 0;
+	            }
+	        }
+	    }
 
-		for (int i = 0; i < this.physicMemory.length; i++) {
-			if (this.physicMemory[i] == null) {
-				actualSize++;
+	    if (bestStart >= 0) {
+	        System.out.println("Writing memory with best fit");
+	        for (int i = bestStart; i < bestStart + p.getSizeInMemory(); i++) {
+	            this.physicMemory[i] = p.getId();
+	            System.out.println(i);
+	        }
+	    } else {
+	        System.out.println("No space available in memory");
+	    }
 
-				if (i == this.physicMemory.length - 1) {
-					bestSize = i - actualSize + 1;
-					if (actualSize < p.getSizeInMemory()) {
-						bestSize = -1;
-
-					}
-				}
-
-			} else {
-
-				if ((actualSize - 1) == p.getSizeInMemory()) {
-					bestSize = i - actualSize + 1;
-					break;
-
-				} else if (actualSize > p.getSizeInMemory() && actualSize < actualBestSize
-						|| i == this.physicMemory.length - 1) {
-
-					bestSize = (i - actualSize) + 1;
-
-				} else if (actualSize > p.getSizeInMemory()) {
-					actualBestSize = (i - actualSize) + 1;
-					bestSize = actualBestSize;
-
-				}
-				actualSize = 0;
-			}
-		}
-		System.out.println();
-		if ( bestSize >= 0) {
-			System.out.println("Writing memory with best fit");
-			int start = bestSize;
-			int end = start + p.getSizeInMemory();
-			AddressMemory address = new AddressMemory(start, end);
-			for (int i = address.getStart(); i < address.getEnd(); i++) {
-				this.physicMemory[i] = p.getId();
-				System.out.println(i);
-			}
-		} else {
-			System.out.println("No space available in memory");
-		}
-		printMemoryStatus();
+	    printMemoryStatus();
 	}
 
-	private void writeWithWorstFit(Process p) {
-		int biggerSize = -1;
-		int actualSize = 0;
-		System.out.println("Process: " + p.getSizeInMemory());
-		for (int i = 0; i < physicMemory.length; i++) {
-			if (this.physicMemory[i] != null) {
-				if (actualSize > biggerSize && actualSize >= p.getSizeInMemory()) {
-					biggerSize = (i - actualSize);
-					actualSize = 0;
-				} else {
-					actualSize = 0;
-				}
-			} else {
-				actualSize++;
-				if (this.physicMemory.length - 1 == i) {
-					if (actualSize > biggerSize) {
-						biggerSize = (i - actualSize) + 1;
-					}
-				}
-			}
-		}
-		if (biggerSize >= 0) {
-			System.out.println("Writing memory with worst fit");
-			int start = biggerSize;
-			int end = start + p.getSizeInMemory();
-			AddressMemory address = new AddressMemory(start, end);
-			for (int i = address.getStart(); i < address.getEnd(); i++) {
-				this.physicMemory[i] = p.getId();
-				System.out.println(i);
-			}
 
-		} else {
-			System.out.println("No space available in memory");
-		}
-		printMemoryStatus();
+
+	private void writeWithWorstFit(Process p) {
+	    int bestStart = -1;
+	    int bestSize = -1;
+	    int currentStart = -1;
+	    int currentSize = 0;
+
+	    for (int i = 0; i < physicMemory.length; i++) {
+	        if (physicMemory[i] == null) {
+	            if (currentStart == -1) {
+	                currentStart = i;
+	            }
+	            currentSize++;
+	        } else {
+	            if (currentSize > bestSize && currentSize >= p.getSizeInMemory()) {
+	                bestSize = currentSize;
+	                bestStart = currentStart;
+	            }
+	            currentStart = -1;
+	            currentSize = 0;
+	        }
+	    }
+
+	    if (currentSize > bestSize && currentSize >= p.getSizeInMemory()) {
+	        bestSize = currentSize;
+	        bestStart = currentStart;
+	    }
+
+	    if (bestStart >= 0) {
+	        System.out.println("Writing memory with worst fit");
+	        for (int i = bestStart; i < bestStart + p.getSizeInMemory(); i++) {
+	            physicMemory[i] = p.getId();
+	            System.out.println(i);
+	        }
+	    } else {
+	        System.out.println("No space available in memory");
+	    }
+
+	    printMemoryStatus();
 	}
 
 	private void writeWithFirstFit(Process p) {
@@ -142,7 +125,7 @@ public class MemoryManager {
 		if (actualSize < p.getSizeInMemory()) {
 			System.out.println("No space available for process");
 		}
-		printMemoryStatus();
+		//printMemoryStatus();
 		
 	}
 
