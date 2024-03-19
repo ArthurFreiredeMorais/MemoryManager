f450e6d207953ef913270ed4f1d66efd668fafee
Arthur Freire de Morais <130699430+ArthurFreiredeMorais@users.noreply.github.com>
2024-03-19 13:49:45 -0300

    Update Execute.java

--- src/so/Execute.java
+++ src/so/Execute.java
@@ -3,19 +3,31 @@ package so;
 public class Execute {
 	public static void main(String[] args) {
 
-		Process p1 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(20, "P1"));
+		Process p1 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(50, "P1"));
 		SystemOperation.systemCall(SystemCallType.WRITE_PROCESS, p1);
-		Process p2 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(38, "P2"));
+		Process p2 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(20, "P2"));
 		SystemOperation.systemCall(SystemCallType.WRITE_PROCESS, p2);
-		Process p3 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(38, "P3"));
+		Process p3 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(30, "P3"));
 		SystemOperation.systemCall(SystemCallType.WRITE_PROCESS, p3);
-		Process p4 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(20, "P4"));
+		Process p4 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(27, "P4"));
 		SystemOperation.systemCall(SystemCallType.WRITE_PROCESS, p4);
-		Process p5 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(8, "P5"));
+		Process p5 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(1, "P5"));
 		SystemOperation.systemCall(SystemCallType.WRITE_PROCESS, p5);
-		Process p6 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(40, "P6"));
+		SystemOperation.systemCall(SystemCallType.CLOSE_PROCESS, p2);
+		SystemOperation.systemCall(SystemCallType.CLOSE_PROCESS, p4);
+		Process p6 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(1, "P6"));
 		SystemOperation.systemCall(SystemCallType.WRITE_PROCESS, p6);
-		SystemOperation.systemCall(SystemCallType.CLOSE_PROCESS, p6);
+		Process p7 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(1, "P7"));
+		SystemOperation.systemCall(SystemCallType.WRITE_PROCESS, p7);
+		Process p8 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(22, "P8"));
+		SystemOperation.systemCall(SystemCallType.WRITE_PROCESS, p8);
+		Process p9 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(17, "P9"));
+		SystemOperation.systemCall(SystemCallType.WRITE_PROCESS, p9);
+		Process p10 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(1, "P10"));
+		SystemOperation.systemCall(SystemCallType.WRITE_PROCESS, p10);
+		Process p11 = SystemOperation.systemCall(SystemCallType.CREATE_PROCESS, new Process(1, "P11"));
+		SystemOperation.systemCall(SystemCallType.WRITE_PROCESS, p11);
+
 		
 	}
 
